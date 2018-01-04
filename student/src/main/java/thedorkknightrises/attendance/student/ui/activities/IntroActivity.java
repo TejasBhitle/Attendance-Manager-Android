bd3d8af704b611dd9bf497645f97e1703b51c121
TheDorkKnightRises <samriddhabasu@gmail.com>
2018-01-04 23:46:34 +0530

    Add a default slide to intro

--- student/src/main/java/thedorkknightrises/attendance/student/ui/activities/IntroActivity.java
+++ student/src/main/java/thedorkknightrises/attendance/student/ui/activities/IntroActivity.java
@@ -3,6 +3,7 @@ package thedorkknightrises.attendance.student.ui.activities;
 import android.os.Bundle;
 
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
 import thedorkknightrises.attendance.student.BuildConfig;
 import thedorkknightrises.attendance.student.R;
@@ -17,6 +18,14 @@ public class IntroActivity extends com.heinrichreimersoftware.materialintro.app.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        addSlide(new SimpleSlide.Builder()
+                .title(R.string.app_name)
+                .description("Welcome")
+                .image(R.mipmap.ic_launcher)
+                .background(R.color.colorPrimary)
+                .backgroundDark(R.color.colorPrimaryDark)
+                .build());
 
         if (BuildConfig.DEBUG) {
             addSlide(new FragmentSlide.Builder()
