03129c35232d0f44d82876d6adfb4dbbc3c0ceed
TejasBhitle <bhitletejas@gmail.com>
2018-01-06 12:24:00 +0530

    [teacher] CourseDetailActivity screen UI

--- teacher/src/main/java/thedorkknightrises/attendance/teacher/ui/activities/CourseDetailActivity.java
+++ teacher/src/main/java/thedorkknightrises/attendance/teacher/ui/activities/CourseDetailActivity.java
@@ -1,16 +1,22 @@
 package thedorkknightrises.attendance.teacher.ui.activities;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import thedorkknightrises.attendance.teacher.Constants;
 import thedorkknightrises.attendance.teacher.R;
-import thedorkknightrises.attendance.teacher.data.AttendanceDbHelper;
-import thedorkknightrises.attendance.teacher.models.BiMap;
 import thedorkknightrises.attendance.teacher.models.Course;
 
 /**
@@ -20,6 +26,12 @@ import thedorkknightrises.attendance.teacher.models.Course;
 public class CourseDetailActivity extends AppCompatActivity {
 
     private Course course;
+    private boolean isEnrollmentOn = true;
+    private CardView enrollment_on_view;
+    private RelativeLayout enrollment_off_view;
+    private TextView lectures_empty_view;
+    private RecyclerView lecturesRecyclerView;
+    private FloatingActionButton lect_add_fab;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,15 +41,87 @@ public class CourseDetailActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         if(bundle != null){
             course = bundle.getParcelable(Constants.COURSE);
-            if(course != null)
-                Toast.makeText(this, course.getName(), Toast.LENGTH_SHORT).show();
         }
 
         setTitle("");
 
         ((TextView)findViewById(R.id.course_name)).setText(course.getName());
         ((TextView)findViewById(R.id.course_info)).setText(course.getInfoText(this));
-        ((TextView)findViewById(R.id.course_desc)).setText(course.getDescription());
+        TextView descTextView = findViewById(R.id.course_desc);
+        String desc = course.getDescription();
+        if(desc.equals("")) descTextView.setVisibility(View.GONE);
+        else descTextView.setText(desc);
+
+        enrollment_on_view = findViewById(R.id.enrollment_on_view);
+        enrollment_off_view = findViewById(R.id.enrollment_off_view);
+        lectures_empty_view = findViewById(R.id.lectures_empty_view);
+        lecturesRecyclerView = findViewById(R.id.lecturesRecyclerView);
+        lect_add_fab = findViewById(R.id.lect_add_fab);
+
+
+        lect_add_fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: intent to add lecture screen
+            }
+        });
+
+        findViewById(R.id.enrolledStudentsButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: intent to students list screen
+            }
+        });
+
+        findViewById(R.id.stop_enrollment).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(CourseDetailActivity.this)
+                        .setTitle(getString(R.string.confirm_stop_enrollment))
+                        .setMessage(getString(R.string.action_undone))
+                        .setPositiveButton(getString(R.string.stop_enrollment), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                stopEnrollment();
+                            }
+                        })
+                        .setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //do nothing
+                            }
+                        })
+                .create()
+                .show();
+
+            }
+        });
+    }
+
+    //TODO: add API call
+    private void stopEnrollment(){
+        Toast.makeText(this,"TODO",Toast.LENGTH_SHORT).show();
+        isEnrollmentOn = false;
+        onResume();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateLayout();
+    }
+
+    private void updateLayout(){
+        if (isEnrollmentOn) {
+            enrollment_on_view.setVisibility(View.VISIBLE);
+            enrollment_off_view.setVisibility(View.GONE);
+            lect_add_fab.setVisibility(View.GONE);
+        }
+        else{
+            enrollment_on_view.setVisibility(View.GONE);
+            enrollment_off_view.setVisibility(View.VISIBLE);
+            lect_add_fab.setVisibility(View.VISIBLE);
+        }
 
     }
 
