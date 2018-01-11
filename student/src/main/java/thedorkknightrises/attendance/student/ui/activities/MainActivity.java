b48867713aea0d69a055d56d5f4a79312dd4847d
TheDorkKnightRises <samriddhabasu@gmail.com>
2018-01-12 00:18:21 +0530

    [student] Allow access even when face has not been scanned (testing)

--- student/src/main/java/thedorkknightrises/attendance/student/ui/activities/MainActivity.java
+++ student/src/main/java/thedorkknightrises/attendance/student/ui/activities/MainActivity.java
@@ -58,8 +58,8 @@ public class MainActivity extends AppCompatActivity
         if (!preferences.getBoolean(Constants.LOGGED_IN, false)) {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
             finish();
-        } if (!preferences.getBoolean(Constants.IS_VIDEO_ADDED, false)) {
-            // TODO: Uncomment when server capable of face recognition
+        // TODO: Uncomment when server capable of face recognition
+        //} if (!preferences.getBoolean(Constants.IS_VIDEO_ADDED, false)) {
             //startActivity(new Intent(MainActivity.this, FaceRecordActivity.class));
             //finish();
         } else {
